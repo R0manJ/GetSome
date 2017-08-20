8f27cce70a70593a1e7fb7cd37ff356ac98fa4b4
R0manJ <zhur0ngjian@126.com>
2017-08-20 18:04:12 +0800

    -完成计算器逻辑 -完成添加页面的选项逻辑设置 -写好了每个页面读取数据的逻辑 -无限滑动逻辑写好 -完成数据存放的逻辑，实现方式是在xml中保存"每天的消费记录集合"的json数据，每次修改集合数据即可。 -准备编写数据存放逻辑 -已经完成viewpage的切换

--- app/src/main/java/com/rjstudio/getsome/AddActivity.java
+++ app/src/main/java/com/rjstudio/getsome/AddActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.RecyclerView;
-import android.text.Layout;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.KeyEvent;
@@ -17,6 +16,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.PopupWindow;
 import android.widget.TextView;
@@ -28,19 +28,33 @@ import com.rjstudio.getsome.bean.DataProvider;
 import com.rjstudio.getsome.bean.OptionalItem;
 import com.rjstudio.getsome.widget.CnToolbar;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AddActivity extends AppCompatActivity {
+public class AddActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Handler popWindowsHandler = new Handler()
     {
         @Override
         public void handleMessage(Message msg) {
+
             cMWindows.showAtLocation(calculateLayout, Gravity.BOTTOM,10,0);
             super.handleMessage(msg);
         }
 
+    };
+    private Handler handlerCalculateLogic = new Handler()
+    {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            tv_amount.setText(msg.obj+"");
+            if (tv_amount.getText().toString().equals(msg.obj+"") && isDecimal)
+            {
+                tv_amount.setText(msg.obj+"");
+            }
+        }
     };
     private View contentView;
     private View calculateLayout;
@@ -53,6 +67,9 @@ public class AddActivity extends AppCompatActivity {
     private RecyclerView rv_setItem;
     private RecyclerView rv_optionalItem;
     private OptionalItemAdapter optionalItemAdapter;
+    private TextView tv_amount;
+    private double tempSava;
+    private int optionKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -108,7 +125,7 @@ public class AddActivity extends AppCompatActivity {
         cMWindows = new PopupWindow(calculateLayout, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT,true);
         cMWindows.setTouchable(true);
         cMWindows.setOutsideTouchable(true);
-        TextView tv_amount = (TextView) findViewById(R.id.tv_setAmount);
+        tv_amount = (TextView) findViewById(R.id.tv_setAmount);
         tv_amount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -132,7 +149,7 @@ public class AddActivity extends AppCompatActivity {
         });
         initRemarkSetLayout();
         initOptionalItemLayout();
-
+        keyboardLogic();
 
     }
     private EditText initRemarkSetLayout()
@@ -168,6 +185,310 @@ public class AddActivity extends AppCompatActivity {
     String TAG = "TEST";
 
 
+    private void keyboardLogic()
+    {
+        Button bu_1 = (Button) calculateLayout.findViewById(R.id.bu_1);
+        Button bu_2 = (Button) calculateLayout.findViewById(R.id.bu_2);
+        Button bu_3 = (Button) calculateLayout.findViewById(R.id.bu_3);
+        Button bu_4 = (Button) calculateLayout.findViewById(R.id.bu_4);
+        Button bu_5 = (Button) calculateLayout.findViewById(R.id.bu_5);
+        Button bu_6 = (Button) calculateLayout.findViewById(R.id.bu_6);
+        Button bu_7 = (Button) calculateLayout.findViewById(R.id.bu_7);
+        Button bu_8 = (Button) calculateLayout.findViewById(R.id.bu_8);
+        Button bu_9 = (Button) calculateLayout.findViewById(R.id.bu_9);
+        Button bu_0 = (Button) calculateLayout.findViewById(R.id.bu_0);
+        Button bu_dop = (Button) calculateLayout.findViewById(R.id.bu_dop);
+        Button bu_del = (Button) calculateLayout.findViewById(R.id.bu_del);
+        Button bu_ac = (Button) calculateLayout.findViewById(R.id.bu_ac);
+        Button bu_equals = (Button) calculateLayout.findViewById(R.id.bu_equals);
+        Button bu_sub = (Button) calculateLayout.findViewById(R.id.bu_sub);
+        Button bu_add = (Button) calculateLayout.findViewById(R.id.bu_add);
+        Button bu_mul = (Button) calculateLayout.findViewById(R.id.bu_mul);
+        Button bu_div = (Button) calculateLayout.findViewById(R.id.bu_div);
+        bu_1.setOnClickListener(this);
+        bu_0.setOnClickListener(this);
+        bu_2.setOnClickListener(this);
+        bu_3.setOnClickListener(this);
+        bu_4.setOnClickListener(this);
+        bu_5.setOnClickListener(this);
+        bu_6.setOnClickListener(this);
+        bu_7.setOnClickListener(this);
+        bu_8.setOnClickListener(this);
+        bu_9.setOnClickListener(this);
+        bu_del.setOnClickListener(this);
+        bu_div.setOnClickListener(this);
+        bu_dop.setOnClickListener(this);
+        bu_sub.setOnClickListener(this);
+        bu_ac.setOnClickListener(this);
+        bu_add.setOnClickListener(this);
+        bu_equals.setOnClickListener(this);
+        bu_mul.setOnClickListener(this);
+    }
+
+    int bit = 1;
+    double number = 0;
+    int key = 0;
+//    number = number + key *10;
+    //1000,100,10,1,0.1,0.01
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.bu_0:
+                calculateLogic(0);
+                break;
+            case R.id.bu_1:
+                calculateLogic(1);
+                Toast.makeText(context, "Onclick + 1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.bu_2:
+                calculateLogic(2);
+                break;
+            case R.id.bu_3:
+                calculateLogic(3);
+                break;
+            case R.id.bu_4:
+                calculateLogic(4);
+                break;
+            case R.id.bu_5:
+                calculateLogic(5);
+                break;
+            case R.id.bu_6:
+                calculateLogic(6);
+                break;
+            case R.id.bu_7:
+                calculateLogic(7);
+                break;
+            case R.id.bu_8:
+                calculateLogic(8);
+                break;
+            case R.id.bu_9:
+                calculateLogic(9);
+                break;
+
+            case R.id.bu_ac:
+                calculateLogic(-1);
+                break;
+            case R.id.bu_dop:
+                Toast.makeText(context, "dop", Toast.LENGTH_SHORT).show();
+                calculateLogic(-2);
+                break;
+            case R.id.bu_del:
+                calculateLogic(-3);
+                break;
+            case R.id.bu_add:
+                calculateLogic(-4);
+                break;
+            case R.id.bu_sub:
+                calculateLogic(-5);
+                break;
+            case R.id.bu_mul:
+                calculateLogic(-6);
+                break;
+            case R.id.bu_div:
+                calculateLogic(-7);
+                break;
+            case R.id.bu_equals:
+                calculateLogic(-8);
+                break;
+        }
+    }
+
+    private boolean isDecimal = false;
+    private double decimalPart= 0f;
+    private int decimalBit = 1;
+    private int integerBit = 1;
+    public void calculateLogic(int key)
+    {
+        if (key == -2)
+        {
+            //Clicked the dop key
+            isDecimal = true;
+
+            return;
+        }
+        else if (key == -3)
+        {
+            if (!isDecimal)
+            {
+                if (integerBit>1)
+                {
+
+//                    number /= 10;
+                    int integer = (int) number;
+                    integer /= 10;
+                    number = integer;
+                    integerBit --;
+                }
+            }
+            else
+            {
+                Log.d(TAG, "calculateLogic: decimalBit = "+decimalBit);
+                int bit10 = 10;
+                for (int i = 1 ; i < (decimalBit-1) ; i ++)
+                {
+                    bit10 *= 10;
+                }
+                if (decimalBit > 1)
+                {
+                    int integer = (int) (number * bit10);
+                    integer /= 10;
+                    number = (double)integer/bit10*10;
+                    decimalBit--;
+                    if (decimalBit == 1) isDecimal=false;
+                }
+            }
+            Message msg = new Message();
+            msg.obj = number;
+            handlerCalculateLogic.sendMessage(msg);
+            return;
+        }
+        else if (key == -4)
+        {
+            //加法逻辑
+            tempSava = number;
+            number = 0;
+            optionKey = 4;
+
+            isDecimal = false;
+            decimalBit = 1;
+            integerBit = 1;
+            Message msg = new Message();
+            msg.obj = number;
+            handlerCalculateLogic.sendMessage(msg);
+            return;
+
+        }
+        else if (key == -5)
+        {
+            tempSava = number;
+            number = 0;
+            optionKey = 5;
+
+            isDecimal = false;
+            decimalBit = 1;
+            integerBit = 1;
+            Message msg = new Message();
+            msg.obj = number;
+            handlerCalculateLogic.sendMessage(msg);
+            return;
 
 
+        }
+        else if (key == -6)
+        {
+            tempSava = number;
+            number = 0;
+            optionKey = 6;
+
+            isDecimal = false;
+            decimalBit = 1;
+            integerBit = 1;
+            Message msg = new Message();
+            msg.obj = number;
+            handlerCalculateLogic.sendMessage(msg);
+            return;
+
+        }
+        else if (key == -7)
+        {
+            tempSava = number;
+            number = 0;
+            optionKey = 7;
+
+            isDecimal = false;
+            decimalBit = 1;
+            integerBit = 1;
+
+            Message msg = new Message();
+            msg.obj = number;
+            handlerCalculateLogic.sendMessage(msg);
+            return;
+        }
+        else if (key == -8)
+        {
+            switch (optionKey)
+            {
+                case 4:
+                    Log.d(TAG, "calculateLogic: add = "+number+"--"+tempSava);
+                    number = BigDecimal.valueOf(tempSava).add(BigDecimal.valueOf(number)).doubleValue();
+                    break;
+                case 5:
+                    number = BigDecimal.valueOf(tempSava).subtract(BigDecimal.valueOf(number)).doubleValue();
+                    break;
+                case 6:
+                    number = BigDecimal.valueOf(tempSava).multiply(BigDecimal.valueOf(number)).doubleValue();
+                    break;
+                case 7:
+                    //除法
+                    number = BigDecimal.valueOf(tempSava).divide(BigDecimal.valueOf(number)).doubleValue();
+                    break;
+            }
+            Message msg = new Message();
+            msg.obj = number;
+            handlerCalculateLogic.sendMessage(msg);
+            return;
+        }
+        else if (key == -1)
+        {
+            //AC
+            number = 0.0f;
+            decimalPart = 0f;
+            decimalBit = 1;
+            integerBit = 1;
+            isDecimal = false;
+
+            optionKey = 0;
+            tempSava = 0;
+            Message msg = new Message();
+            msg.obj = number;
+            handlerCalculateLogic.sendMessage(msg);
+            return;
+
+        }
+
+        if (!isDecimal )
+        {
+
+            if ( integerBit >= 9) return;
+            number = (number * 10) + key ;
+            integerBit ++;
+
+        }
+        else
+        {
+
+            //小数的表达方式
+            int beforeChangeToDecimal = 0;
+            //0.1 , 0.01 , 0.001
+            if (decimalBit <= 3)
+            {
+
+                int bit10 = 10;
+                Log.d(TAG, "calculateLogic: bit :"+decimalBit);
+                for (int i = 1 ; i < decimalBit ;i++)
+                {
+                    bit10 *= 10;
+                    Log.d(TAG, "calculateLogic: bit = "+bit10);
+                }
+                beforeChangeToDecimal = (int) (number * bit10)/bit10;
+//                Log.d(TAG, "calculateLogic: beforeChangeToDEcimal (1)+"+beforeChangeToDecimal);
+                beforeChangeToDecimal = beforeChangeToDecimal * bit10 + key;
+//                Log.d(TAG, "calculateLogic: beforeChangeToDEcimal (2)+"+beforeChangeToDecimal);
+                decimalPart = (double)(beforeChangeToDecimal % bit10) / bit10;
+//                Log.d(TAG, "calculateLogic: bigDecimal:"+bigDecimal);
+//                Log.d(TAG, "calculateLogic: beforeChangeToDEcimal (3)+"+decimalPart);
+                Log.d(TAG, "calculateLogic: "+BigDecimal.valueOf(decimalPart).doubleValue());
+                number = BigDecimal.valueOf(decimalPart).add(BigDecimal.valueOf(number)).doubleValue();
+                Log.d(TAG, "calculateLogic: number "+number);
+                decimalBit ++;
+            }
+
+        }
+
+        Message msg = new Message();
+        msg.obj = number;
+        handlerCalculateLogic.sendMessage(msg);
+//        bit ++;
+    }
 }
